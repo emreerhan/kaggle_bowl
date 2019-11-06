84ccb4ec10b852179429d8d419030a7a81379a5f
Emre Erhan <eerhan@bcgsc.ca>
2019-11-05 16:53:14 -0800

    Initial commit. Add cookiecutter directories

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='An attempt at the Kaggle Data Science Bowl 2019',
+    author='MOM_GENES_STU',
+    license='MIT',
+)
